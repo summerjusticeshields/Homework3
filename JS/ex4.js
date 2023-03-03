f7171d7042af682e09c30a86bdbd9cf3e6051fda
Summer <summer.shields01@student.csulb.edu>
2023-03-02 17:20:51 -0800

    Commit 3/2/2023 5:20pm

--- JS/ex4.js
+++ JS/ex4.js
@@ -1,9 +1,58 @@
-//4. 
-const num_a = Number(prompt('Enter a number:'))
-const num_b = Number(prompt('Enter another number:'))
+const studentList = [
+    {
+        firstName: "Allan",
+        lastName: "Able",
+        scores: [95, 85, 92, 98]
+    },
+    {
+        firstName: "Amy",
+        lastName: "Alexander",
+        scores: [80, 88, 100]
+    },
+    {
+        firstName: "Betty",
+        lastName: "Barns",
+        scores: [70, 80, 90, 100]
+    },
+    {
+        firstName: "Bob",
+        lastName: "Bones",
+        scores: [75, 85, 95, 85]
+    },
+    {
+        firstName: "Cindy",
+        lastName: "Chase",
+        scores: [95, 90, 92, 98]
+    },
+    {
+        firstName: "Charles",
+        lastName: "Chips",
+        scores: [88, 99, 90]
+    },
+  
+  ];
 
-console.log(`Addition: ${num_a} plus ${num_b} =`, num_a + num_b)
-console.log(`Subtraction: ${num_a} minus ${num_b} =`, num_a - num_b)
-console.log(`Multiplication: ${num_a} times ${num_b} =`, num_a * num_b)
-console.log(`Division: ${num_a} divided by ${num_b} =`, num_a / num_b)
-console.log(`Modulo: ${num_a} modulo ${num_b} =`, num_a % num_b)
+const newStudentList = studentList.map(student => {
+    const newScores = student.scores.map(score => score + 5);
+    return {
+      firstName: student.firstName,
+      lastName: student.lastName,
+      scores: newScores
+    };
+  });
+
+  const LastNameResults = studentList
+  .map(student => {
+    const minScore = Math.min(...student.scores);
+    const maxScore = Math.max(...student.scores);
+    const avgScore = student.scores.reduce((total, score) => total + score) / student.scores.length;
+    return {
+      firstName: student.firstName,
+      lastName: student.lastName,
+      minScore: minScore,
+      maxScore: maxScore,
+      avgScore: avgScore
+    };
+  });
+  
+  console.log(newStudentList);
